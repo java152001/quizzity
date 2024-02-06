622297891db32e0064a55dc1e2a66ea6f35b2eaf
java152001 <java152001@gmail.com>
2024-02-06 11:45:47 -0700

    Updated start script for development

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const app = express();
 const port = 3001;
 const knexConfig = require('./quizzityDB/knexfile');
 
-const knex = require('knex')(knexConfig[process.env.NODE_ENV]);
+const knex = require('knex')(knexConfig[process.env.NODE_ENV.trim()]);
 
 app.use(bodyParser.json());
 
 app.post('/api/quiz/create', (req, res) => {
     const postData = req.body;
 
+    let quizData = {
+        title: "Let Us Begin",
+        quizStart: postData.date,
+        joinID: 123456
+    }
+
     try {
-        const ids = knex('quizzes').insert(postData);
+        const ids = knex('quizzes').insert(quizData);
         res.status(201).json(ids);
     } catch (err) {
         res.status(500).json({ message: "Error creating new post", error: err })
